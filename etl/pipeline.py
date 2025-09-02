1c67235e77801d100afe15ab0fcd752d057a15b5
arperez22 <aperez21@huskers.unl.edu>
2025-09-01 21:03:05 -0500

    Fixed pipeline imports

--- etl/pipeline.py
+++ etl/pipeline.py
@@ -1,6 +1,6 @@
-from extract import extract_data
-from transform import transform_data
-from load import load_data
+from .extract import extract_data
+from .transform import transform_data
+from .load import load_data
 
 
 def run_pipeline(conn):
