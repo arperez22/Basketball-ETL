b24403bb7440d12713fa401e9595452aaa8c8df1
arperez22 <aperez21@huskers.unl.edu>
2025-09-12 22:57:03 -0500

    Adjusted formatting and primary key column names

--- etl/load.py
+++ etl/load.py
@@ -1,5 +1,3 @@
-
-
 def load_data(conn):
     create_tables(conn)
     load_teams(conn)
@@ -11,7 +9,7 @@ def create_tables(conn):
     cursor = conn.cursor()
 
     cursor.execute("""  CREATE TABLE IF NOT EXISTS teams (
-                        team_id             INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                        id                  INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                         name                VARCHAR(50) NOT NULL UNIQUE,
                         record              VARCHAR(50) NOT NULL,
                         wins                INT NOT NULL,
@@ -26,48 +24,48 @@ def create_tables(conn):
                     ); """)
     
     cursor.execute("""  CREATE TABLE IF NOT EXISTS players (
-                        player_id       INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-                        name            VARCHAR(50) NOT NULL,
-                        jersey_number   INT NOT NULL,
-                        class           VARCHAR(2) NULL,
-                        position        VARCHAR(1) NOT NULL,
-                        height          VARCHAR(10) NULL,
-                        weight          REAL NULL,
-                        hometown        VARCHAR(150) NULL,
-                        high_school     VARCHAR(150) NULL,
-                        team            VARCHAR(50),
+                        id                  INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                        name                VARCHAR(50) NOT NULL,
+                        jersey_number       INT NOT NULL,
+                        class               VARCHAR(2) NULL,
+                        position            VARCHAR(1) NOT NULL,
+                        height              VARCHAR(10) NULL,
+                        weight              REAL NULL,
+                        hometown            VARCHAR(150) NULL,
+                        high_school         VARCHAR(150) NULL,
+                        team                VARCHAR(50),
 
                         CONSTRAINT fk_team FOREIGN KEY (team) REFERENCES teams(name) ON DELETE CASCADE
                     ); """)
 
     cursor.execute("""  CREATE TABLE IF NOT EXISTS player_stats (
-                        stats_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-                        player_id INT NOT NULL,
-                        games_played INT,
-                        games_started INT,
-                        minutes_played NUMERIC(3, 1),
-                        field_goals NUMERIC(3, 1),
-                        field_goals_attempted NUMERIC(3, 1),
-                        field_goal_percentage NUMERIC(4, 3) DEFAULT 0,
-                        three_points NUMERIC(3, 1),
-                        three_points_attempted NUMERIC(3, 1),
-                        three_point_percentage NUMERIC(4, 3) DEFAULT 0,
-                        two_points NUMERIC(3, 1),
-                        two_points_attempted NUMERIC(3, 1),
-                        two_point_percentage NUMERIC(4, 3) DEFAULT 0,
+                        id                              INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                        player_id                       INT NOT NULL,
+                        games_played                    INT,
+                        games_started                   INT,
+                        minutes_played                  NUMERIC(3, 1),
+                        field_goals                     NUMERIC(3, 1),
+                        field_goals_attempted           NUMERIC(3, 1),
+                        field_goal_percentage           NUMERIC(4, 3) DEFAULT 0,
+                        three_points                    NUMERIC(3, 1),
+                        three_points_attempted          NUMERIC(3, 1),
+                        three_point_percentage          NUMERIC(4, 3) DEFAULT 0,
+                        two_points                      NUMERIC(3, 1),
+                        two_points_attempted            NUMERIC(3, 1),
+                        two_point_percentage            NUMERIC(4, 3) DEFAULT 0,
                         effective_field_goal_percentage NUMERIC (4, 3) DEFAULT 0,
-                        free_throws NUMERIC(3, 1),
-                        free_throws_attempted NUMERIC(3, 1),
-                        free_throws_percentage NUMERIC(4, 3) DEFAULT 0,
-                        offensive_rebounds NUMERIC(3, 1),
-                        defensive_rebounds NUMERIC(3, 1),
-                        total_rebounds NUMERIC(3, 1),
-                        assists NUMERIC(3, 1),
-                        steals NUMERIC(3, 1),
-                        blocks NUMERIC(3, 1),
-                        turnovers NUMERIC(3, 1),
-                        personal_fouls NUMERIC(3, 1),
-                        points_per_game NUMERIC(3, 1),
+                        free_throws                     NUMERIC(3, 1),
+                        free_throws_attempted           NUMERIC(3, 1),
+                        free_throws_percentage          NUMERIC(4, 3) DEFAULT 0,
+                        offensive_rebounds              NUMERIC(3, 1),
+                        defensive_rebounds              NUMERIC(3, 1),
+                        total_rebounds                  NUMERIC(3, 1),
+                        assists                         NUMERIC(3, 1),
+                        steals                          NUMERIC(3, 1),
+                        blocks                          NUMERIC(3, 1),
+                        turnovers                       NUMERIC(3, 1),
+                        personal_fouls                  NUMERIC(3, 1),
+                        points_per_game                 NUMERIC(3, 1),
                    
                         CONSTRAINT fk_player FOREIGN KEY (player_id) REFERENCES players(player_id) ON DELETE CASCADE
                     ); """)    
